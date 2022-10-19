3ff9059e62a0206aca462906b2e0b34e8e002f6d
Meng Yao <myao3@slb.com>
2022-10-19 13:52:32 +0800

    feat: add  Dto/ExpenseItem.cs

---
+++ ExpenseRecord/Dto/ExpenseItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseRecord.Dto;
+
+public class ExpenseItem
+{
+    [Column("description")]
+    [Required]
+    [MaxLength(50)]
+    public string Description { get; set; }
+
+    [Column("type")]
+    [Required]
+    [MaxLength(50)]
+    public string Type { get; set; }
+
+    [Column("amount")]
+    [Required]
+    public double Amount { get; set; }
+
+    [Column("date")]
+    public DateOnly Date { get; set; } = new DateOnly(0,0,0);
+}
