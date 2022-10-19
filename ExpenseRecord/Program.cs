4696cc9ca0c34ba5f66611a11987f93ab977c347
Meng Yao <myao3@slb.com>
2022-10-19 15:48:07 +0800

    feat: add Controllers/ExpenseController.cs; integration tests passed for 2 versions of services.

--- ExpenseRecord/Program.cs
+++ ExpenseRecord/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ExpenseRecord;
+using ExpenseRecord.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+builder.Services.AddScoped<IExpenseItemService, ExpenseItemService>();
 
 var app = builder.Build();
 
